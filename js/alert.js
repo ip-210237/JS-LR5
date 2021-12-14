88643d3a85f72f6ec1fd342002038adcd43fa17f
ip-210237 <96092500+ip-210237@users.noreply.github.com>
2021-12-14 09:08:49 +0200

    Bugs fixed

--- js/alert.js
+++ js/alert.js
@@ -8,5 +8,5 @@ let fullName = `${firstName} ${lastName}`;
 let yearOfBirth = 2004;
 let currentYear = new Date().getFullYear();
 
-window.alert(`Привіт, мене звуть ${firstName}! Я студент групи ${group}.`);
+window.alert(`Привіт, мене звуть ${fullName}! Я студент групи ${group}.`);
 window.alert(`Мені ${currentYear - yearOfBirth} років.`);
