b36a272b2a9f5341ad1d3bf7d83a4e96246e8f6d
ip-210237 <96092500+ip-210237@users.noreply.github.com>
2021-12-14 03:19:03 +0200

    Adding files to repo

---
+++ js/alert.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let firstName = "Едуард";
+let lastName = "Зяблін";
+let group = "IP-21-2";
+let fullName = `${firstName} ${lastName}`;
+
+let yearOfBirth = 2004;
+let currentYear = new Date().getFullYear();
+
+window.alert(`Привіт, мене звуть ${firstName}! Я студент групи ${group}.`);
+window.alert(`Мені ${currentYear - yearOfBirth} років.`);
